20d81148ed91e93f994ecc8e75a4bd9fd475e14f
abdelkarim <abdelkarimksouri@gmail.com>
2020-04-02 17:51:50 +0200

    add my first controller for OC_project with route and twig

---
+++ src/Controller/AdvertController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+class AdvertController
+{
+
+    public function index(Environment $twig)
+    {
+        $content = $twig->render('Advert/index.html.twig');
+
+
+        return new Response($content);
+    }
+
+}
